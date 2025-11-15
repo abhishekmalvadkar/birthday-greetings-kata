328859a7ac662f7f06ac15bd221bd78abaa85e22
Abhishek <abhi@dev.com>
2025-11-15 12:57:04 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/bgk/BirthdayGreetingsKataApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.bgk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayGreetingsKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayGreetingsKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/bgk/BirthdayGreetingsKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.bgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayGreetingsKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
