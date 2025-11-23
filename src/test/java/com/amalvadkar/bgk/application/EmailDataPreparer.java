bb68f4c791a500160f730a75d64815c99d8829c9
Abhishek <abhi@dev.com>
2025-11-23 16:02:08 +0530

    Given     1) Non Birthday friend's firstname     2) List of birthday friend's full names When we call prepareBirthdayReminder() function with above params from EmailDataPreparer Then it should return EmailData value object with below props     1) Subject : Birthday Reminder     2) Body :         Dear <non_birthday_friend_first_name>,
            Today is <birthday_friend_1_full_name>, <birthday_friend_2_full_name>'s birthday.
            Don't forget to send them a message !

--- src/test/java/com/amalvadkar/bgk/application/EmailDataPreparer.java
+++ src/test/java/com/amalvadkar/bgk/application/EmailDataPreparer.java
@@ -3,6 +3,9 @@ package com.amalvadkar.bgk.application;
 import com.amalvadkar.bgk.domain.EmailData;
 import com.amalvadkar.bgk.domain.FullName;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EmailDataPreparer {
     public EmailData prepare(String firstname) {
         String subject = "Happy birthday!";
@@ -19,5 +22,22 @@ public class EmailDataPreparer {
                 Don't forget to send him a message !
                 """, nonBirthDayFriendFirstName, birthFriendFullName.asString());
         return EmailData.from(subject, body);
+    }
+
+    public EmailData prepareBirthdayReminder(String nonBirthDayFriendFirstName, List<FullName> birthdayFriendsFullNames) {
+        String subject = "Birthday Reminder";
+        String body = String.format("""
+                Dear %s,
+                
+                Today is %s's birthday.
+                Don't forget to send them a message !
+                """, nonBirthDayFriendFirstName, joinedWithComma(birthdayFriendsFullNames));
+        return EmailData.from(subject, body);
+    }
+
+    private static String joinedWithComma(List<FullName> birthdayFriendsFullNames) {
+        return birthdayFriendsFullNames.stream()
+                .map(FullName::asString)
+                .collect(Collectors.joining(", "));
     }
 }
--- src/test/java/com/amalvadkar/bgk/application/EmailDataPreparerTest.java
+++ src/test/java/com/amalvadkar/bgk/application/EmailDataPreparerTest.java
@@ -4,6 +4,8 @@ import com.amalvadkar.bgk.domain.EmailData;
 import com.amalvadkar.bgk.domain.FullName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EmailDataPreparerTest {
@@ -26,6 +28,20 @@ public class EmailDataPreparerTest {
                 
                 Today is John Doe's birthday.
                 Don't forget to send him a message !
+                """);
+    }
+
+    @Test
+    void should_prepare_email_data_for_birthday_reminder_of_more_then_one_friend() {
+        EmailDataPreparer emailDataPreparer = new EmailDataPreparer();
+        List<FullName> birthdayFriendsFullNames = List.of(FullName.from("John", "Doe"), FullName.from("Tom", "Poe"));
+        EmailData emailData = emailDataPreparer.prepareBirthdayReminder("Mary", birthdayFriendsFullNames);
+        assertThat(emailData.subject()).isEqualTo("Birthday Reminder");
+        assertThat(emailData.body()).isEqualTo("""
+                Dear Mary,
+                
+                Today is John Doe, Tom Poe's birthday.
+                Don't forget to send them a message !
                 """);
     }
 }
