b1f2c75516a29070141109c053c615bf671ba2a5
Abhishek <abhi@dev.com>
2025-11-23 16:13:28 +0530

    Refactored test

--- src/test/java/com/amalvadkar/bgk/application/EmailDataPreparerTest.java
+++ src/test/java/com/amalvadkar/bgk/application/EmailDataPreparerTest.java
@@ -2,6 +2,7 @@ package com.amalvadkar.bgk.application;
 
 import com.amalvadkar.bgk.domain.EmailData;
 import com.amalvadkar.bgk.domain.FullName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,18 +11,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class EmailDataPreparerTest {
 
+    public static final String BIRTHDAY_FRIEND_FIRST_NAME = "John";
+    public static final String NON_BIRTH_DAY_FRIEND_FIRST_NAME = "Mary";
+    public static final FullName SINGLE_BIRTHDAY_FRIEND_FULL_NAME = FullName.from("John", "Doe");
+    public static final List<FullName> MANY_BIRTHDAY_FRIENDS_FULL_NAMES = List.of(FullName.from("John", "Doe"), FullName.from("Tom", "Poe"));
+
+    private EmailDataPreparer emailDataPreparer;
+
+    @BeforeEach
+    void setUp() {
+        emailDataPreparer = new EmailDataPreparer();
+    }
+
     @Test
     void should_prepare_email_data_for_birthday_greeting_email() {
-        EmailDataPreparer emailDataPreparer = new EmailDataPreparer();
-        EmailData emailData = emailDataPreparer.prepare("John");
+        EmailData emailData = emailDataPreparer.prepare(BIRTHDAY_FRIEND_FIRST_NAME);
+
         assertThat(emailData.subject()).isEqualTo("Happy birthday!");
         assertThat(emailData.body()).isEqualTo("Happy birthday, dear John!");
     }
 
     @Test
     void should_prepare_email_data_for_birthday_reminder_of_single_friend() {
-        EmailDataPreparer emailDataPreparer = new EmailDataPreparer();
-        EmailData emailData = emailDataPreparer.prepareBirthdayReminder("Mary", FullName.from("John", "Doe"));
+        EmailData emailData = emailDataPreparer.prepareBirthdayReminder(NON_BIRTH_DAY_FRIEND_FIRST_NAME, SINGLE_BIRTHDAY_FRIEND_FULL_NAME);
+
         assertThat(emailData.subject()).isEqualTo("Birthday Reminder");
         assertThat(emailData.body()).isEqualTo("""
                 Dear Mary,
@@ -33,9 +46,8 @@ public class EmailDataPreparerTest {
 
     @Test
     void should_prepare_email_data_for_birthday_reminder_of_more_then_one_friend() {
-        EmailDataPreparer emailDataPreparer = new EmailDataPreparer();
-        List<FullName> birthdayFriendsFullNames = List.of(FullName.from("John", "Doe"), FullName.from("Tom", "Poe"));
-        EmailData emailData = emailDataPreparer.prepareBirthdayReminder("Mary", birthdayFriendsFullNames);
+        EmailData emailData = emailDataPreparer.prepareBirthdayReminder(NON_BIRTH_DAY_FRIEND_FIRST_NAME, MANY_BIRTHDAY_FRIENDS_FULL_NAMES);
+
         assertThat(emailData.subject()).isEqualTo("Birthday Reminder");
         assertThat(emailData.body()).isEqualTo("""
                 Dear Mary,
